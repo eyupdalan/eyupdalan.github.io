22f6a0f5c6afb6395c152f43fc7f76d1bceb005a
Edalan <Edalan@ziraatteknoloji.com>
2019-03-08 17:12:30 +0300

    eyup adds static page

---
+++ precache-manifest.5475b83aa86bff60838b6ba4a8c43bf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e72ccb0e29c1360f32b",
+    "url": "/static/css/main.f269518a.chunk.css"
+  },
+  {
+    "revision": "6e72ccb0e29c1360f32b",
+    "url": "/static/js/main.1b89014c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d7da3b07bd85af9f5a34",
+    "url": "/static/css/2.5ac18a4e.chunk.css"
+  },
+  {
+    "revision": "d7da3b07bd85af9f5a34",
+    "url": "/static/js/2.9dc95145.chunk.js"
+  },
+  {
+    "revision": "0139a10c752a4adcf0852bd2b8616090",
+    "url": "/index.html"
+  }
+];
